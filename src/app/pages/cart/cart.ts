54dfad01c221378fe240827d4c04ae5c3ea701ad
kostyasabada <ksabada@mycubes.nl>
2025-06-10 16:12:44 +0300

    add summary

--- src/app/pages/cart/cart.ts
+++ src/app/pages/cart/cart.ts
@@ -1,17 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { CartService } from '../../services/cart';
 import { CartItem } from "./cart-item/cart-item";
+import { OrderSummary } from "./order-summary/order-summary";
 
 @Component({
   selector: 'app-cart',
-  imports: [CartItem],
+  imports: [CartItem, OrderSummary],
   template: `
     <div class="p-6 flex flex-col gap-4">
       <h2 lass="text-2x1">Shopping Cart</h2>
       @for (item of cartService.cart(); track item.id) {
         <app-cart-item [item]="item" />
       }
-      
+      <app-order-summary />
     </div>
   `,
   styles: ``
